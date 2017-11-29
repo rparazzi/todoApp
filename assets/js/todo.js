a05c676611e62a16b2b92f5648fa3eae59a07971
parazzi <rparazzi@gmail.com>
2017-11-28 23:21:54 -0200

    animation

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -9,7 +9,8 @@ function Todo () {
         $("ul").on("click", "li", this.completeTask);
         $("ul").on("click" ,".delete", this.deleteTask);
         $("ul").on("click", ".btnControl", this.timeTrack);
-        $("#newTodo").keypress(this.createTask)
+        $("#newTodo").keypress(this.createTask);
+        $(".fa-plus-square-o").click(this.createTaskEffect);
     };
 
     this.deleteTask = function (event) {
@@ -22,7 +23,8 @@ function Todo () {
     this.createTask = function (event) {
         //only add if user press enter
         if ( event.which === 13) {
-            $("ul").append("<li strTime=\"0\"><span class=\"delete\">X</span> "+$(this).val()+"</li>");
+            //$("ul").append("<li strTime=\"0\"><span class=\"delete\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> "+$(this).val()+"<span class=\"btnControl\"> > </span><span class=\"time\">0</span></li>");
+            $("ul").append("<li strTime='0'><span class='delete'><i class='fa fa-trash' aria-hidden='true'></i></span>" + $(this).val() +"<span class='btnControl'><i class='fa fa-clock-o' aria-hidden='true'></i></span><span class='time'>0s</span></li>")
             $(this).val("");
         }
     };
@@ -33,16 +35,24 @@ function Todo () {
       event.stopPropagation();
       if ( $(this).parent().attr("strTime") === "0" ) {
         $(this).parent().attr("strTime",Date.now());
+        $(this).parent().find(".fa-clock-o").toggleClass("pause");
       } else {
         var actualValue = parseInt($(this).parent().find(".time").text());
         var strTime = $(this).parent().attr("strTime");
         var elTime = Math.floor((Date.now() - strTime)/1000);
         var rsp = actualValue + elTime
 
-        $(this).parent().find(".time").text(rsp);
+        $(this).parent().find(".time").text(rsp+"s");
         $(this).parent().attr("strTime",0);
+        $(this).parent().find(".fa-clock-o").toggleClass("pause");
+        
       }
     };
+
+    this.createTaskEffect = function () { 
+        $("#newTodo").fadeToggle();
+    };
+
 };
 
 //create Todo page object.
